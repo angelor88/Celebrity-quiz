77e84472778b9177c02e1a52e7e49c50912ad476
Angelo Russoniello and Dillon Titcomb <angelo@>
2018-01-11 15:29:46 -0800

    Adds photos for all celebrities and properly hides and shows each of them when new information is provided.

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,23 @@ $(document).ready(function() {
     var age = $("select#age").val();
     var gender = $("select#gender").val();
     var lifestyle = $("select#lifestyle").val();
+    $('.celeb').hide();
     if (gender === 'male' && age === 'senior' && lifestyle === 'wildchild') {
       $('#donaldTrump').show();
-    // } else if (gender === 'male' && age === 'senior' && lifestyle === 'nihilistic') {
-    //     $('#morganFreeman').show;
-    // }  else if (gender === 'male' && age === 'adult' && lifestyle === 'wildchild') {
-    //     $
-    // }  else if (gender ===  && age ===  && lifestyle === ) {
-    // }  else if (gender ===  && age ===  && lifestyle === ) {
-    // }  else if (gender ===  && age ===  && lifestyle === ) {
-    } else {
+    } else if (gender === 'male' && age === 'senior' && lifestyle === 'nihilistic') {
+      $('#morganFreeman').show();
+    }  else if (gender === 'male' && age === 'adult' && lifestyle === 'wildchild') {
+      $('#joaquinPhoenix').show();
+    }  else if (gender ==='male' && age === 'adult' && lifestyle === 'nihilistic') {
+      $('#kanyeWest').show();
+    }  else if (gender === 'female' && age === 'senior' && lifestyle === 'wildchild') {
+      $('#merylStreep').show();
+    }  else if (gender === 'female' && age === 'senior' && lifestyle === 'nihilistic') {
+      $('#judiDench').show();
+    }  else if (gender === 'female' && age === 'adult' && lifestyle === 'wildchild') {
+      $('#selenaGomez').show();
+    }  else {
+      $('#kristenStewart').show();
     }
     event.preventDefault();
   });
